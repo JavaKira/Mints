9d8e25f985fb8655dc17f02ed98d885cf203e040
JavaKira <JavaKira@yandex.ru>
2022-04-06 22:42:54 +0300

    Init commit

---
+++ src/main/java/com/mints/MintsApplication.java
@@ -0,0 +1,11 @@
+package com.mints;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MintsApplication.class, args);
+    }
+}

1a06e05680752248199ee939ce171baeb8cfaa73
JavaKira <JavaKira@yandex.ru>
2022-04-12 14:36:54 +0300

    Added Product price

--- src/main/java/com/mints/entity/Product.java
+++ src/main/java/com/mints/entity/Product.java
@@ -19,6 +19,8 @@ public class Product {
     private String description;
     private String thumbnailPath;
 
+    private int price;
+
     public Product(String name, String description, String thumbnailPath) {
         this.name = name;
         this.description = description;

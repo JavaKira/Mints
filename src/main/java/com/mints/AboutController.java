94ebaa94aa0ed1cdc96094670a9ba5bf8da61817
JavaKira <JavaKira@yandex.ru>
2022-04-17 08:57:31 +0300

    Added about page

---
+++ src/main/java/com/mints/AboutController.java
@@ -0,0 +1,14 @@
+package com.mints;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("about")
+public class AboutController {
+    @GetMapping
+    public String getIndexPage() {
+        return "about/index";
+    }
+}
